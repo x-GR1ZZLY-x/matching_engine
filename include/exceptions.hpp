e4863f9454943d7a66860efb9bab52ffa21923f1
newlu <newluwilife@gmail.com>
2026-08-09 22:01:02 +0300

    feat: add exceptions hierarchy

---
+++ include/exceptions.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace matching_engine {
+
+class MatchingEngineError : public std::runtime_error {
+public:
+    explicit MatchingEngineError(const std::string& message)
+        : std::runtime_error(message) {}
+};
+
+class OrderError : public MatchingEngineError{
+public:
+    explicit OrderError(const std::string& message)
+        : MatchingEngineError(message) {}
+};
+
+class ParseError : public MatchingEngineError{
+public:
+    explicit ParseError(const std::string& message)
+        : MatchingEngineError(message) {}
+};
+
+class DuplicateOrderError : public MatchingEngineError{
+public:
+    explicit DuplicateOrderError(const std::string& message)
+        : MatchingEngineError(message) {}
+};
+
+class OrderBookError : public MatchingEngineError{
+public:
+    explicit OrderBookError(const std::string& message)
+        : MatchingEngineError(message) {}
+};
+
+}
